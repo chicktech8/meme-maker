a941c7c90a9da903936eeab7dd4af104f8e784ff
chicktech8 <chicktechies+8@gmail.com>
2017-12-10 10:11:47 -0600

    my changes

---
+++ app/src/main/java/com/example/hlupico/mememaker/ya_ok.java
@@ -0,0 +1,13 @@
+package com.example.hlupico.mememaker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ya_ok extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ya_ok);
+    }
+}
---
+++ app/src/main/java/com/example/hlupico/mememaker/yeah_ok.java
@@ -0,0 +1,13 @@
+package com.example.hlupico.mememaker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class yeah_ok extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yeah_ok);
+    }
+}
